e77383de5d13c0fae98abf1bb4802349fc6250c1
Bytan97 <enbsf@outlook.com>
2020-05-22 14:06:06 +0300

    13) Прогноз через Python

---
+++ 13) Прогноз через Python/file.py
@@ -0,0 +1,49 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+base = 'localhost:5432/base'
+name_pass = 'postgres:50541'
+pref = 'postgresql'
+engine = create_engine(f"{pref}://{name_pass}@{base}")
+
+
+def moving_average(d1: str, d2: str, window_size: int) -> pd.DataFrame:
+    SQL = '''
+        SELECT 
+            recept.client cl, recgoods.goods g, recept.ddate d, sum(recgoods.volume * recgoods.price) s 
+        FROM 
+            recept JOIN recgoods ON (recept.id = recgoods.id) 
+        WHERE 
+            recept.ddate >= %(mindate)s AND recept.ddate <= %(maxdate)s
+        GROUP BY 
+        recept.client, recgoods.goods, recept.ddate
+        ORDER BY
+        recept.ddate;
+    '''
+    df = pd.read_sql(SQL,
+                     engine,
+                     params={'mindate': d1, 'maxdate': d2},
+                     parse_dates={'recept.ddate': dict(format='%Y%m%d'), }
+                     )
+    N = df.shape[0]
+    if (N < window_size):
+        raise ValueError(f"Invalid windows size > {N}")
+
+    dfs = df.set_index(['cl', 'g'])
+    dfs.drop('d', axis=1, inplace=True)
+    dfs = dfs.groupby(level=['cl', 'g']).rolling(
+        window=window_size,  min_periods=1).mean()
+    dfs.reset_index(level=[2, 1], inplace=True)
+    dfs.reset_index(drop=True, inplace=True)
+    dft = df.sort_values(['cl', 'g'],)
+    dft.reset_index(drop=True, inplace=True)
+    names = ['client', 'goods', 'date', 'sum']
+    dft.columns = names
+    dft['prediction'] = dfs['s']
+    dft = dft.sort_values('date')
+
+    return dft
+
+
+result = moving_average('20200102', '20201231', 2)
+print(result.head(30))

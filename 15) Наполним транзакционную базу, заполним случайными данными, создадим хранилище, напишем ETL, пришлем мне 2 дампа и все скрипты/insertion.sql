025d4e4d6e4972c0f6589cbf222001c36b4ba231
Bytan97 <enbsf@outlook.com>
2020-05-24 23:13:04 +0300

    Изменение скриптов на создание

--- 15) Наполним транзакционную базу, заполним случайными данными, создадим хранилище, напишем ETL, пришлем мне 2 дампа и все скрипты/insertion.sql
+++ 15) Наполним транзакционную базу, заполним случайными данными, создадим хранилище, напишем ETL, пришлем мне 2 дампа и все скрипты/insertion.sql
@@ -26,7 +26,7 @@ FROM generate_series(1, 5) t;
 INSERT INTO goods_groups(name, parent)
 SELECT ('Subgroup: ' || t)::text,
        (SELECT id FROM goods_groups WHERE t > 0 ORDER BY random() LIMIT 1)
-FROM generate_series(1, 10) t;
+FROM generate_series(1, 5) t;
 
 INSERT INTO goods(g_group, name, weight, length, height, width)
 SELECT (SELECT id FROM goods_groups WHERE t > 0 ORDER BY random() LIMIT 1),
@@ -35,7 +35,7 @@ SELECT (SELECT id FROM goods_groups WHERE t > 0 ORDER BY random() LIMIT 1),
        (SELECT * FROM generate_series(1, 1488) WHERE t > 0 ORDER BY random() LIMIT 1),
        (SELECT * FROM generate_series(1, 50) WHERE t > 0 ORDER BY random() LIMIT 1),
        (SELECT * FROM generate_series(1, 54) WHERE t > 0 ORDER BY random() LIMIT 1)
-FROM generate_series(1, 150) t;
+FROM generate_series(1, 15) t;
 
 CREATE OR REPLACE FUNCTION generate_dates(dt1 date,
                                           dt2 date,
@@ -50,7 +50,7 @@ SELECT (SELECT * FROM generate_dates('2020-01-01', '2020-12-31', 2) WHERE t > 0 
        t,
        (SELECT client.id FROM client WHERE t > 0 ORDER BY random() LIMIT 1),
        (SELECT storage.id FROM storage WHERE t > 0 ORDER BY random() LIMIT 1)
-FROM generate_series(1, 1000) t
+FROM generate_series(1, 100000) t
 ORDER BY d;
 
 INSERT INTO recept(ddate, ndoc, client, storage)
@@ -58,7 +58,7 @@ SELECT (SELECT * FROM generate_dates('2020-01-01', '2020-12-31', 1) WHERE t > 0 
        t,
        (SELECT client.id FROM client WHERE t > 0 ORDER BY random() LIMIT 1),
        (SELECT storage.id FROM storage WHERE t > 0 ORDER BY random() LIMIT 1)
-FROM generate_series(1, 1000) t
+FROM generate_series(1, 100000) t
 ORDER BY d;
 
 INSERT INTO income(ddate, ndoc, client, storage)
@@ -66,7 +66,7 @@ SELECT (SELECT * FROM generate_dates('2020-01-01', '2020-12-31', 1) WHERE t > 0 
        t,
        (SELECT client.id FROM client WHERE t > 0 ORDER BY random() LIMIT 1),
        (SELECT storage.id FROM storage WHERE t > 0 ORDER BY random() LIMIT 1)
-FROM generate_series(1, 1000) t
+FROM generate_series(1, 100000) t
 ORDER BY d;
 
 
@@ -76,7 +76,7 @@ SELECT t,
        (SELECT goods.id FROM goods WHERE t > 0 ORDER BY random() LIMIT 1),
        (SELECT * FROM generate_series(1, 500) WHERE t > 0 ORDER BY random() LIMIT 1),
        (SELECT * FROM generate_series(1, 1500) WHERE t > 0 ORDER BY random() LIMIT 1)
-FROM generate_series(1, 1000) t;
+FROM generate_series(1, 100000) t;
 
 INSERT INTO recgoods(id, subid, goods, volume, price)
 SELECT t,
@@ -84,29 +84,29 @@ SELECT t,
        (SELECT goods.id FROM goods WHERE t > 0 ORDER BY random() LIMIT 1),
        (SELECT * FROM generate_series(1, 500) WHERE t > 0 ORDER BY random() LIMIT 1),
        (SELECT * FROM generate_series(1, 1500) WHERE t > 0 ORDER BY random() LIMIT 1)
-FROM generate_series(1, 1000) t;
+FROM generate_series(1, 100000) t;
 
 
 INSERT INTO bank_income(ddate, summ, client)
 SELECT R.ddate, (R2.volume * R2.price) sum, R.client
 FROM recept R
          JOIN recgoods R2 ON R.id = R2.id
-LIMIT 500;
+LIMIT 50000;
 
 INSERT INTO cassa_income(ddate, summ, client)
 SELECT R.ddate, (R2.volume * R2.price) sum, R.client
 FROM recept R
          JOIN recgoods R2 ON R.id = R2.id
-LIMIT 500 OFFSET 500;
+LIMIT 50000 OFFSET 50000;
 
 INSERT INTO bank_recept(ddate, summ, client)
 SELECT I.ddate, (I2.volume * I2.price) sum, I.client
 FROM income I
          JOIN incgoods I2 ON I.id = I2.id
-LIMIT 500;
+LIMIT 50000;
 
 INSERT INTO cassa_recept(ddate, summ, client)
 SELECT I.ddate, (I2.volume * I2.price) sum, I.client
 FROM income I
          JOIN incgoods I2 ON I.id = I2.id
-LIMIT 500 OFFSET 500;
+LIMIT 50000 OFFSET 50000;

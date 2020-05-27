4d61e53c02759e2b4213a83280f15a78159df53f
Bytan97 <enbsf@outlook.com>
2020-05-27 19:50:21 +0300

    Update purchase.sql

--- 15) Наполним транзакционную базу, заполним случайными данными, создадим хранилище, напишем ETL, пришлем мне 2 дампа и все скрипты/purchase.sql
+++ 15) Наполним транзакционную базу, заполним случайными данными, создадим хранилище, напишем ETL, пришлем мне 2 дампа и все скрипты/purchase.sql
@@ -1,4 +1,4 @@
-DROP SCHEMA IF EXISTS purchase CASCADE ;
+DROP SCHEMA IF EXISTS purchase CASCADE;
 CREATE SCHEMA purchase;
 SET search_path = "purchase";
 
@@ -35,16 +35,17 @@ CREATE TABLE IF NOT EXISTS goods
 );
 CREATE TABLE purchase
 (
-    id         serial PRIMARY KEY,
-    date_id    int REFERENCES date (id),
-    client_id  int REFERENCES client (id),
-    goods_id   int REFERENCES goods (id),
-    storage_id int REFERENCES storage (id),
+    id             serial PRIMARY KEY,
+    date_id        int REFERENCES date (id),
+    client_id      int REFERENCES client (id),
+    goods_id       int REFERENCES goods (id),
+    storage_id     int REFERENCES storage (id),
+    purchase_price int,
 
-    count      int,
-    sum        int,
-    volume     int,
-    weight     int
+    count          int,
+    sum            int,
+    volume         int,
+    weight         int
 );
 
 INSERT INTO date (date)
@@ -64,11 +65,12 @@ INSERT INTO storage (name)
 SELECT S.name
 FROM public.storage S;
 
-INSERT INTO purchase (date_id, client_id, goods_id, storage_id, sum, count, volume, weight)
+INSERT INTO purchase (date_id, client_id, goods_id, storage_id, purchase_price, sum, count, volume, weight)
 SELECT D.id,
        I1.client,
        I2.goods,
        I1.storage,
+       I2.price,
 
        sum(I2.volume * I2.price),
        I2.volume,
@@ -78,4 +80,4 @@ FROM public.income I1
          JOIN public.incgoods I2 ON I1.id = I2.id
          JOIN goods G ON G.id = I2.goods
          JOIN date D ON D.date = I1.ddate
-GROUP BY D.id, I1.client, I2.goods, I1.storage, I2.volume, G.item_weight;
+GROUP BY D.id, I1.client, I2.goods, I1.storage, I2.price, I2.volume, I2.volume * G.item_weight;
